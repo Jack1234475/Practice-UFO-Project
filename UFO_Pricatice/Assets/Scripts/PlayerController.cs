d34d8df0ac65d6ed6fb5f629a962a1332a3b348c
Joaquin Alonzo <joaquinalonzo22@yahoo.com>
2019-02-17 17:01:47 -0800

    Player: Movement and speed

---
+++ UFO_Pricatice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rd2d;
+
+    private void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rd2d.AddForce(movement * speed);
+    }
+}
